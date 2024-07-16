244958ef3856e9a7d92476ab2f3f4876921e3a5f
mscharistang <157583831+mscharistang@users.noreply.github.com>
2024-07-16 22:36:27 +0800

    Update mainprog.py
    change emp4 number

--- mainprog.py
+++ mainprog.py
@@ -1,5 +1,5 @@
 from empClass import Employee
-emp4 = Employee("Xavier", "Ho", "M77777")
+emp4 = Employee("Xavier", "Ho", "M777555")
 emp5 = Employee("Lex", "Deng", "M654321")
 print(emp4.first_name)
 print(emp5.first_name)
